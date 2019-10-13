0e3e31e0d209854602bcfba26fff05a74a5dacb0
Alina <stereoalina@gmail.,com>
2019-10-13 20:28:44 +0300

    Изменены названия в меню

--- views/layouts/header.php
+++ views/layouts/header.php
@@ -13,13 +13,13 @@
     <script type="text/javascript" src="/template/js/main.js"></script>
 </head>
 <body class="d-flex flex-column">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light justify-content-center mb-3">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a id="list_menu_item" class="nav-link" href="/">Список задач <i class="fas fa-list"></i></a>
+                <a id="list_menu_item" class="nav-link" href="/">Все задачи <i class="fas fa-list"></i></a>
             </li>
             <li class="nav-item">
-                <a id="create_menu_item" class="nav-link" href="task/create">Добавить задачу <i class="fas fa-plus"></i></a>
+                <a id="create_menu_item" class="nav-link" href="/task/create">Создать задачу <i class="fas fa-plus"></i></a>
             </li>
             <li class="login_menu_item">
                 <a id="" class="nav-link" href="#">Вход для админа <i class="fas fa-sign-in-alt"></i></a>
